6f3ad25abbd49ec8f38cb01963df2c36381eb148
ardalanfp <aforoughipour@gmail.com>
2019-02-06 12:46:57 +0330

    first commit

---
+++ src/main/java/com/microservice/FumElection/ElectionPortalApplication.java
@@ -0,0 +1,11 @@
+package com.microservice.FumElection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionPortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElectionPortalApplication.class, args);
+    }
+}
